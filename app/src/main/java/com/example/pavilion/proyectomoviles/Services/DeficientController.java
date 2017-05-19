0e02253bdb3b3283f77101b92605d981b005d2ec
bryan-ruiz <bryanrb95@gmail.com>
2017-05-18 19:46:10 -0600

    version mejorada
    tiene retrofit a medias

--- app/src/main/java/com/example/pavilion/proyectomoviles/Services/DeficientController.java
+++ app/src/main/java/com/example/pavilion/proyectomoviles/Services/DeficientController.java
@@ -1,50 +1,45 @@
 package com.example.pavilion.proyectomoviles.Services;
 
 
-
-import com.squareup.okhttp.OkHttpClient;
-
-import java.util.ArrayList;
+import android.widget.Toast;
 
 import retrofit2.Call;
 import retrofit2.Callback;
-import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 /**
  * Created by Pavilion on 18/5/2017.
  */
 public class DeficientController {
     private DeficientServiceFactory service;
+    private final Retrofit retrofit;
+
     private String API_ROOT = " https://sheltered-headland-93521.herokuapp.com";
 
+
     public DeficientController() {
-//        RestAdapter.Builder builder = new RestAdapter.Builder()
-//                .setEndpoint(API_ROOT)
-//                .setClient(new OkClient(new OkHttpClient()));
-//        wscServicio = builder.build().create(WebServiceConn.class);
-//
-//    }
-
-    private ArrayList<DeficientModel> index(){
-        ArrayList<DeficientModel> res;
-        service.getDeficients(new Callback<ArrayList<DeficientModel>>() {
-            @Override
-            public void onResponse(Call<ArrayList<DeficientModel>> call, Response<ArrayList<DeficientModel>> response) {
-
-            }
-
-            @Override
-            public void onFailure(Call<ArrayList<DeficientModel>> call, Throwable throwable) {
-
-            }
-        });
+        this.retrofit = new Retrofit.Builder().baseUrl(API_ROOT).addConverterFactory(GsonConverterFactory.create()).build();
+        this.service = retrofit.create(DeficientServiceFactory.class);
     }
+    /*Call<estudiante> call = connectionManager.getMainInterface().getStudent(carnetTextView.getText().toString(),
+            Integer.parseInt(mPasswordView.getText().toString()));
+                call.enqueue(new Callback<odontologia.proyectoodontologia.estudiante>() {
+        @Override
+        public void onResponse(Call<estudiante> call, Response<estudiante> response) {
+            Student student = Student.getInstance();
+            student.FillInformation(response.body().getCarne(), response.body().getPin(), String.valueOf(response.body().getBecado()),
+                    response.body().getNombre(), response.body().getApe1(), response.body().getApe2(),
+                    response.body().getCarrera(), response.body().getEstadoCivil(), response.body().getCarneCCSS(),
+                    response.body().getFechaNacimiento(), response.body().getCedula(), response.body().getTelefono());
+            Toast.makeText(LoginActivity.this, "Bienvenido " + student.getNombre(), Toast.LENGTH_SHORT).show();
+            startActivity(MainActivityIntent);
+        }
 
-
-
-
-
-
-
+        @Override
+        public void onFailure(Call<estudiante> call, Throwable t) {
+            Toast.makeText(LoginActivity.this, "Error de inicio de sesión", Toast.LENGTH_SHORT).show();
+        }
+    });*/
 
 }
